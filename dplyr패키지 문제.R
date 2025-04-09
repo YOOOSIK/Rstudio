d0352a9bcfeba36d9f8627cc6a49114c24b4d8bc
yo1sik <wsooy12@gmail.com>
2025-04-09 10:58:26 +0900

    Add files via upload

---
+++ dplyr패키지 문제.R
@@ -0,0 +1,15 @@
+# dplyr 패키지 설치 및 로드하기(p.207)
+install.packages("dplyr")
+library(dplyr)
+
+nrow(mtcars)
+str(mtcars)
+
+cyl_group <- group_by(mtcars, cyl)
+
+sorted_cars<-arrange(cyl_group , cyl, desc(hp))
+sorted_cars <- mutate(mtcars, hp_rank = rank(hp),cyl=rank(cyl))
+sorted_cars
+print(sorted_cars, n=32)
+arrange(sorted_cars, desc(hp))
+
